e8220f446a48da9020a5187f2c33c4c5af3b863e
Acceleration17 <acceleration17@gmail.com>
2024-03-04 10:53:51 -0500

    March 3rd

---
+++ my-portfolio/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import './index.css'; // Or wherever your main CSS file is located
+
+function App() {
+  return (
+    <div className="w-96 text-black text-6xl font-bold font-['Roboto'] leading-10">
+      From Control System to Embodied AI
+    </div>
+  );
+}
+
+export default App;
